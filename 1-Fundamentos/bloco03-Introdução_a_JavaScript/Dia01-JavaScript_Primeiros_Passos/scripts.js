9bd938869aad10a9e5ff5f95bf3b253f253d5850
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-01-24 13:43:49 -0300

    padronização do nome das pastas

---
+++ 1-Fundamentos/bloco03-Introdução_a_JavaScript/Dia01-JavaScript_Primeiros_Passos/scripts.js
@@ -0,0 +1,17 @@
+const num1 = 37
+const num2 = 05
+const num3 = 13
+
+/* if (num1 > num2) {
+    console.log(num1)
+} else {
+    console.log(num2)
+} */
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1)
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2)
+} else {
+    console.log(num3)
+}
