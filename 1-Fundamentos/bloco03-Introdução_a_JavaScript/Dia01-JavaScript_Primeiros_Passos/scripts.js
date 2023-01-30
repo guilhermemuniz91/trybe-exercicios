55e1a71877f6c0c980cdfa96b9e4569e8459e2bb
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-01-29 21:18:33 -0300

    exercícios do dia01

--- 1-Fundamentos/bloco03-Introdução_a_JavaScript/Dia01-JavaScript_Primeiros_Passos/scripts.js
+++ 1-Fundamentos/bloco03-Introdução_a_JavaScript/Dia01-JavaScript_Primeiros_Passos/scripts.js
@@ -1,17 +1,42 @@
-const num1 = 37
-const num2 = 05
-const num3 = 13
+const num1 = 60
+const num2 = 65
+const num3 = 55
 
-/* if (num1 > num2) {
-    console.log(num1)
-} else {
-    console.log(num2)
-} */
+// 1)
+// if (num1 > num2) {
+//     console.log(num1)
+// } else {
+//     console.log(num2)
+// }
 
-if (num1 > num2 && num1 > num3) {
-    console.log(num1)
-} else if (num2 > num1 && num2 > num3) {
-    console.log(num2)
-} else {
-    console.log(num3)
-}
+
+// 2)
+// if (num1 > num2 && num1 > num3) {
+//     console.log(num1)
+// } else if (num2 > num1 && num2 > num3) {
+//     console.log(num2)
+// } else {
+//     console.log(num3)
+// }
+
+
+// 3)
+// if (num1 > 0) {
+//     console.log('positive');
+// }   else if (num1 < 0) {
+//     console.log('negative');
+// } else console.log('zero');
+
+
+// 4)
+// let triangulo = num1 + num2 + num3
+// if (triangulo == 180) {
+//     console.log('true');
+// } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+//     console.log('mensagem de erro')
+// } else {
+//     console.log('false');
+// }
+
+
+// 5) Xadrez
